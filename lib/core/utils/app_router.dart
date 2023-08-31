96aaeb892846843ace5da723ab7ecc60bc80f7e9
AlyaaTalaat <alyaatalaat205@gmail.com>
2023-08-31 10:48:38 +0300

    first commit

---
+++ lib/core/utils/app_router.dart
@@ -0,0 +1,13 @@
+import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
+import 'package:go_router/go_router.dart';
+
+abstract class AppRouter{
+  static final router=GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) =>const SplashView(),
+        ),
+    ]
+  );
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'core/utils/app_router.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+     routerConfig: AppRouter.router,
+     debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
