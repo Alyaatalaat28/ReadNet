8338d5b5dff92e2871fdf3d0396c1b76fd75961d
AlyaaTalaat <alyaatalaat205@gmail.com>
2023-09-01 12:07:02 +0300

    custom app bar

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -2,9 +2,11 @@ import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../features/home/presentation/views/home_view.dart';
+import '../../features/search/presentation/views/search_view.dart';
 
 abstract class AppRouter{
   static const  kHomeView='/HomeView';
+  static const  kSearchView='/SearchView';
   static final router=GoRouter(
     routes: [
       GoRoute(
@@ -14,7 +16,12 @@ abstract class AppRouter{
         GoRoute(
           path: kHomeView,
           builder: (context, state) =>const HomeView(),
+          ),
+          GoRoute(
+            path: kSearchView,
+            builder: (context, state) => const SearchView(),
           )
+
     ]
   );
 }
---
+++ lib/features/home/presentation/views/widgets/custom_appbar.dart
@@ -0,0 +1,36 @@
+import 'package:bookly_app/core/utils/assets_data.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../../../../core/utils/app_router.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Padding(
+      padding: const EdgeInsets.only(top: 40,bottom: 20),
+      child: Row(
+        children: [
+         const Image(
+          image: AssetImage(
+            AssetsData.logo,
+            ),
+            height: 18.0,
+            ),
+         const Spacer(),
+          IconButton(
+            onPressed: (){
+              GoRouter.of(context).push(AppRouter.kSearchView);
+            }, 
+            icon:const Icon(
+              FontAwesomeIcons.magnifyingGlass,
+              size: 22.0,
+              )),
+        ],
+      ),
+    );
+  }
+}
