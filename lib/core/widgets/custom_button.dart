4267bed3cef554a72d52bb209a3baabee0fd362b
AlyaaTalaat <alyaatalaat205@gmail.com>
2023-09-01 21:21:16 +0300

    custom button

---
+++ lib/core/widgets/custom_button.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import '../utils/styles.dart';
+
+
+class CustomButton extends StatelessWidget {
+   CustomButton({
+    super.key,
+    required this.text, 
+    this.fontSize,
+    this.onPressed,
+    required this.textColor,
+    required this.backgroundColor,
+    this.borderRadius
+    });
+    final Color textColor;
+    final String text;
+    final double ?fontSize;
+    final void Function()? onPressed;
+    final Color backgroundColor;
+    final  BorderRadius? borderRadius;
+  @override
+  Widget build(BuildContext context) {
+    return  SizedBox(
+      height: 48,
+      child: TextButton(
+        onPressed: onPressed,
+        style: TextButton.styleFrom(
+          backgroundColor:backgroundColor ,
+          shape: RoundedRectangleBorder(
+            borderRadius: borderRadius??BorderRadius.circular(12),
+          )
+        ),
+         child: Text(text,
+         style: Styles.textStyle18.copyWith(
+          color: textColor,
+          fontWeight:FontWeight.w900 ,
+          fontSize:fontSize ,
+         ),
+         )),
+    );
+  }
+}
---
+++ lib/features/home/presentation/views/widgets/book_details_section.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BookDetailsSection extends StatelessWidget {
+  const BookDetailsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
