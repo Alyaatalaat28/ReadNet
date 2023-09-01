12634315727d38d0fb8d9092c32d6f3e265c427e
AlyaaTalaat <alyaatalaat205@gmail.com>
2023-09-01 20:17:08 +0300

    home view body

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,10 +1,45 @@
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+
+import 'best_seller_listview.dart';
+import 'custom_appbar.dart';
+import 'featured_book_listview.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const CustomScrollView(
+      physics: BouncingScrollPhysics(),
+      slivers: [
+        SliverToBoxAdapter(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children:  [
+             Padding(
+               padding: EdgeInsets.symmetric(horizontal: 30),
+               child: CustomAppBar(),
+             ),
+             FeaturedBookListView(),
+             SizedBox(height: 50.0,),
+             Padding(
+               padding:EdgeInsets.symmetric(horizontal: 30),
+               child: Text('Newst Books',
+               style: Styles.textStyle18,),
+             ),
+             SizedBox(height: 20.0,)
+          ],
+        ),
+        ),
+        SliverFillRemaining(
+          child:Padding(
+            padding:EdgeInsets.symmetric(horizontal: 30),
+            child: BestSellerListView(),
+          ) ,
+        ),
+      ],
+
+    );
   }
 }
