8c5b73541584c61f270c4a63cb210df469494e2b
AlyaaTalaat <alyaatalaat205@gmail.com>
2023-09-01 17:40:26 +0300

    custom book image

---
+++ lib/features/home/presentation/views/widgets/custom_book_image.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/assets_data.dart';
+
+class CustomBookImage extends StatelessWidget {
+  const CustomBookImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  ClipRRect(
+      borderRadius: BorderRadius.circular(16.0),
+      child:const AspectRatio(
+        aspectRatio: 2.6/4,
+        child: Image(
+          image: AssetImage(AssetsData.testImage),
+          fit: BoxFit.fill,
+          ),
+        ),
+    );
+  }
+}
