636bc2194559cf32432523756e799de8285e130b
AlyaaTalaat <alyaatalaat205@gmail.com>
2023-09-07 15:32:28 +0300

    handle failure

---
+++ lib/core/errors/failure.dart
@@ -0,0 +1,50 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:dio/dio.dart';
+
+abstract class Failure {
+  final String errMessage;
+
+  const Failure(this.errMessage);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.errMessage);
+
+  factory ServerFailure.fromDioError(DioError dioError) {
+    switch (dioError.type) {
+      case DioErrorType.connectionTimeout:
+        return ServerFailure('Connection timeout with ApiServer');
+
+      case DioErrorType.sendTimeout:
+        return ServerFailure('Send timeout with ApiServer');
+
+      case DioErrorType.receiveTimeout:
+        return ServerFailure('Receive timeout with ApiServer');
+
+      case DioErrorType.cancel:
+        return ServerFailure('Request to ApiServer was canceld');
+
+      case DioErrorType.unknown:
+        if (dioError.message!.contains('SocketException')) {
+          return ServerFailure('No Internet Connection');
+        }
+        return ServerFailure('Unexpected Error, Please try again!');
+      default:
+       return ServerFailure.fromResponse(
+            dioError.response!.statusCode, dioError.response!.data);
+    }
+  }
+factory ServerFailure.fromResponse(int? statusCode,dynamic response){
+  if(statusCode==400||statusCode==401||statusCode==403){
+    return ServerFailure(response['error']['message']);
+  }else if(statusCode==400){
+    return ServerFailure('Your request not found, Please try later!');
+  }else if(statusCode==500){
+    return ServerFailure('Internal Server error, Please try later');
+  }else{
+    return ServerFailure('Opps There was an Error, Please try again');
+  }
+}
+  
+}
