b9fc239659e69e6ead1d5bcee9285cf30ffc3371
AlyaaTalaat <alyaatalaat205@gmail.com>
2023-09-09 11:41:35 +0300

    custom snack bar

---
+++ lib/core/utils/function/custom_snack_bar.dart
@@ -0,0 +1,13 @@
+
+
+import 'package:flutter/material.dart';
+
+void customSnackBar(context ,String text){
+  ScaffoldMessenger.of(context).showSnackBar(
+   SnackBar(
+    content: Text(
+      text,
+      )
+    )
+  );
+}
